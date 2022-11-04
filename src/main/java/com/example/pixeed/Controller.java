aed3b3c9d54ac8b2454f0b0c3d6ff730eda77abf
shivansh005 <shivanshshuklasultanpur@gmail.com>
2022-11-04 20:17:51 +0530

    Adding text on image functionality with icons

--- src/main/java/com/example/pixeed/Controller.java
+++ src/main/java/com/example/pixeed/Controller.java
@@ -49,6 +49,38 @@ public class Controller {
         });
 
     }
+
+@FXML
+    private void ADDBUTTONPRESSED() {
+        Text text = new Text();
+        text.setText(FIELD.getText());
+        text.setFill(COLORPICKER.getValue());
+        text.setStyle("-fx-font: 50 arial");
+        text.setStyle("-fx-background-color: white");
+        text.setLayoutX(imageViewPane.getWidth() / 2);
+        text.setLayoutY(imageViewPane.getHeight() / 2);
+        text.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                text.setLayoutX(mouseEvent.getSceneX());
+                text.setLayoutY(mouseEvent.getSceneY());
+            }
+        });
+        imageViewPane.getChildren().add(text);
+        COLORPICKER.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                text.setFill(COLORPICKER.getValue());
+            }
+        });
+        FONTADJUST.valueProperty().addListener(new InvalidationListener() {
+            @Override
+            public void invalidated(Observable observable) {
+                text.setFont(new Font(FONTADJUST.getValue()));
+            }
+        });
+    }
+    
  @FXML
     protected void SETRAIN2() throws FileNotFoundException {
         Filters filters = new Filters();
