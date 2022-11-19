5ba6cae55c59fa8343757f4a94c71d0d01a702e2
shivansh005 <shivanshshuklasultanpur@gmail.com>
2022-11-19 17:33:42 +0530

    Adding module-info. java file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,11 @@ module com.example.pixeed
 {
     requires javafx.controls;
     requires javafx.fxml;
-    opens com.example.pixeed to javafx.fxml;
+    requires javafx.graphics;
+    requires java.desktop;
+    requires javafx.swing;
+    requires opencv;
+    requires com.jfoenix;
     exports com.example.pixeed;
+    opens com.example.pixeed to javafx.fxml;
 }
